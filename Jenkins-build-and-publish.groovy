589b888b3941b0322b902d2d70dd76d269d56e90
Cesuuur <cesar.redondourdiales@telefonica.com>
2023-02-17 12:54:08 +0100

    more test!

--- Jenkins-build-and-publish.groovy
+++ Jenkins-build-and-publish.groovy
@@ -64,7 +64,8 @@ pipeline {
                     rm -rf $SERVICE_NAME 
                     mkdir $SERVICE_NAME 
                     cd $SERVICE_NAME
-                    ls | echo
+                    git credentialsId: '11b0c311-eb71-4277-a430-4071de1d8c82', url: 'git@github.com:Telefonica/pesp-dcip-microbackend.git'
+                    cd pesp-dcip-microbackend
                     '''
                 }
                 // git clone --single-branch --branch $GIT_HUB_BRANCH $GIT_HUB_URL .
